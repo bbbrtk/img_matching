fc29781b25c761141b5d21a5922ddc44c38c0ec8
bbbrtk <sobkowiak.bart@gmail.com>
2020-04-15 21:19:24 +0200

    basic matching

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from scipy import ndimage
 import matplotlib.pyplot as plt
+import collections
 
 def parser():
     """ Parse arguments """
@@ -12,15 +13,8 @@ def parser():
 
     return parser.parse_args()
 
-"""
-TODO
-1. find approx right angles (2) -> abs(x-90)
-2. find bottom base (long line)
-3.  for whole_len of bottom_base: 
-        find a black/white point above
-        calculate length
-        create sth like chart to check if its correct
-"""
+def switch(a, b):
+    return b, a
 
 def generate_rectangle(contours):
     rect = cv2.minAreaRect(contours[0])
@@ -44,15 +38,21 @@ def rotate(contours, img):
         img = ndimage.rotate(img, 90)
     return img
 
-def print_plot(some_list):
-    print(some_list)
-    plt.plot(range(len(some_list)), some_list)
+def print_plot(slist, name, type):
+    # print(slist)
+    plt.plot(range(len(slist)), slist)
+    # plt.savefig(f'matches/{name}{type}.png')
+    # plt.clf()
     plt.show()
 
-def extract_top_bottom(img):
+def extract_top_bottom(img, direction):
     bottom = []
     top = []
-    for pixel in range(0, img.shape[1]-1, 1):
+
+    if direction == 'to_right':   img_width = range(0, img.shape[1]-1, 1)
+    elif direction == 'to_left':  img_width = range(img.shape[1]-1, 0, -1)
+
+    for pixel in img_width:
         white_points = np.where(img[:, pixel] == [255,255,255])[0]
 
         if len(white_points) > 0:
@@ -72,9 +72,27 @@ def extract_top_bottom(img):
 
     return bottom, top
 
-def match(imglist):
-    img = imglist[0]
-    
+def calculate_st_dev(slist):
+    slist = list(filter(lambda a: a != 0, slist))
+    slist.remove(max(slist))
+    slist.remove(min(slist))
+    stdev = np.std(slist)
+    return stdev
+
+def get_top_coords(img, direction):
+    switched = False
+    # extract
+    bottom, top = extract_top_bottom(img, direction)
+    # rotate upside-down if necessary
+    stdev_bottom = calculate_st_dev(bottom)
+    stdev_top = calculate_st_dev(top)
+    if stdev_bottom > stdev_top:
+        switched = True
+        top, bottom = switch(top, bottom)
+
+    return top, switched
+
+def matching_pair(img):
     # find first contours
     imggray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     contours, _ = cv2.findContours(imggray, 2, cv2.CHAIN_APPROX_SIMPLE)
@@ -88,24 +106,61 @@ def match(imglist):
     img = img * 0
     draw_contours(contours, img)
 
-    # get statistics
-    bottom, top = extract_top_bottom(img)
-    print_plot(top)
-    print_plot(bottom)
+    # save
+    # cv2.imwrite('test_image.png',img)
+    # cv2.imshow('ImageWindow', img)
+    # cv2.waitKey()
 
+    # get coords of upper part of a possible pair-image
+    img_pair = img
+    top_pair, switched_pair = get_top_coords(img_pair, 'to_left')
 
-    # tab = tab - min(tab)
+    # get coords of upper part of this image
+    img_me = cv2.flip(img, -1)
+    top_me, switched_me = get_top_coords(img_me, 'to_left')
+
+    # if top-bottom were switched
+    if switched_pair:
+        top_me, top_pair = switch(top_me, top_pair)
+
+    # print_plot(top_pair, 'TEST','pair')
+    # print_plot(top_me, 'TEST', 'me')
+
+    return top_me, top_pair
+
+def squeez(slist):
+    slist = list(filter(lambda a: a != 0, slist))
+    slist.remove(max(slist))
+    slist = np.array_split(slist, 50)
+    mean_slist = []
+    for e in slist: mean_slist.append(np.mean(e))
+    return mean_slist
+
+def match(imglist):
+    images = []
+    pairs = []
+    for i in range(len(imglist)):
+        top_me, top_pair = matching_pair(imglist[i])
+        
+        mean_me = squeez(top_me)
+        mean_pair = squeez(top_pair)
+
+        # print_plot(mean_me, i, 'me')
+        # print_plot(mean_pair, i, 'pair')
+
+        images.append(mean_me)
+        pairs.append(mean_pair)
+
+    for img in images:
+        all_matches = {}
+        for p in range(len(pairs)):
+            val = np.std(np.subtract(img, pairs[p]))
+            all_matches[p] = val
+        all_sorted = sorted(all_matches.items(), key=lambda kv: kv[1])
+        sorted_dict = collections.OrderedDict(all_sorted)
+        print(*list(sorted_dict.keys()))
+        
     
-    # save image
-    cv2.imwrite('test_image.png',img)
-    print(img.shape)
-
-
-
-
-
-
-
 
     matching_order = "1"
     return matching_order
